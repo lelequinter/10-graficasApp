973e0da9d6a33b662f23683bf8b0fef4d095f97f
Leyder Quintero <lele.quinter@gmail.com>
2022-10-27 14:57:41 -0500

    Añadir flexibilidad a nuestro componente personalizado

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 
@@ -10,6 +10,9 @@ import { BaseChartDirective } from 'ng2-charts';
 })
 export class GraficaBarraComponent implements OnInit {
 
+  @Input() horizontal: boolean = false;
+  @Input() labels: string[] = [];
+  @Input() chartData!: any;
 
   @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
 
@@ -31,17 +34,34 @@ export class GraficaBarraComponent implements OnInit {
   public barChartType: ChartType = 'bar';
 
   public barChartData: ChartData<'bar'> = {
-    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
-    datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: '#ED0E0C' },
-      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor: '#E000D9' },
-      { data: [38, 28, 60, 39, 96, 47, 50], label: 'Series C', backgroundColor: '#760CED' }
-    ]
+    labels: this.labels,
+    datasets: this.chartData,
+    // [
+    //   { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: '#ED0E0C' },
+    //   { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor: '#E000D9' },
+    //   { data: [38, 28, 60, 39, 96, 47, 50], label: 'Series C', backgroundColor: '#760CED' }
+    // ]
   };
 
   constructor() { }
 
   ngOnInit(): void {
+    if (this.horizontal) {
+      this.barChartOptions = {
+        indexAxis: 'y'
+      }
+    }
+
+    this.barChartData = {
+      labels: this.labels,
+      datasets: this.chartData,
+      // [
+      //   { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: '#ED0E0C' },
+      //   { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor: '#E000D9' },
+      //   { data: [38, 28, 60, 39, 96, 47, 50], label: 'Series C', backgroundColor: '#760CED' }
+      // ]
+    };
+
   }
 
 }
