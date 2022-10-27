5f41d5d26a5ea98e1260cec76dde2bed7a43d4cb
Leyder Quintero <lele.quinter@gmail.com>
2022-10-27 15:13:18 -0500

    Gráfica de Dona

--- src/app/graficas/pages/dona/dona.component.ts
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartData, ChartType, Color } from 'chart.js';
 
 @Component({
   selector: 'app-dona',
@@ -6,11 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DonaComponent implements OnInit {
+export class DonaComponent {
 
-  constructor() { }
+  colors: Color[] = ['#F7AC20','#7C0AF5','#F7208E']
 
-  ngOnInit(): void {
-  }
+  // Doughnut
+  public doughnutChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: [350, 450, 100], backgroundColor: this.colors },
+      // { data: [50, 150, 120] },
+      // { data: [250, 130, 70] }
+    ]
+  };
+  public doughnutChartType: ChartType = 'doughnut';
+
+
+
 
 }
